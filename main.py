653c5461d06282271964a33a0aa2231bf1118d01
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-03-17 23:50:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("Creation main.py")
+from jira import JIRA
+jira = JIRA(basic_auth=('shona1', '<api token>'),
+            options={"server": "https://jira.digital.ingka.com/", "verify": False}, validate=True)
+
+test_case_data = {
+    "project": "CDOP",
+    "summary": "test Bug",
+    "description": "Bug description",
+    "issuetype": {'name': 'Bug'}}
+jira.create_issue(fields=test_case_data)
+
+if __name__ == "__main__":
+    JIRA()
+    jira.create_issue(fields=test_case_data)
